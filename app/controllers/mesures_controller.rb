37f3735ae03b508538607847a191db793add641a
Martín Cantarini <md.cantarini@gmail.com>
2017-01-21 12:25:13 -0300

    fix estadisticas

--- app/controllers/mesures_controller.rb
+++ app/controllers/mesures_controller.rb
@@ -2,18 +2,31 @@ class MesuresController < ApplicationController
 
   def statics
     id = params[:user_id]
-    cant = 10
-    cant2 = 50
 
-    last_minute_mesures = Mesure.where(user_id: id).last(cant)
-    last_minute_mesures_count = last_minute_mesures.count
-    last_minute_mesures_prom = last_minute_mesures.sum(&:value)
-    if (last_minute_mesures_count != 0 &&  last_minute_mesures_count < cant)
-      cant = last_minute_mesures_count
-    end  
-    last_minute_mesures_prom /= cant
-   
+    #TODAS LAS MEDICIONES DE ID
     mesures = Mesure.where(user_id: id)
+    
+    #ULTIMO MINUTO
+    last_minute_mesures = mesures.where(created_at: (Time.now - 1.minutes)..Time.now)
+    last_minute_mesures_count = last_minute_mesures.count
+    last_minute_mesures_sum = last_minute_mesures.sum(&:value) 
+    if last_minute_mesures_count > 0
+      last_minute_mesures_prom = last_minute_mesures_sum / last_minute_mesures_count 
+    else
+      last_minute_mesures_prom = 0   
+    end
+
+    #ULTIMOS 5 MINUTOS
+    last_5_minutes_mesures = mesures.where(created_at: (Time.now - 5.minutes)..Time.now)
+    last_5_minutes_mesures_count = last_5_minutes_mesures.count
+    last_5_minutes_mesures_sum = last_5_minutes_mesures.sum(&:value)
+    if last_5_minutes_mesures_count > 0
+      last_5_minute_mesures_prom = last_5_minute_mesures_sum / last_5_minute_mesures_count
+    else
+      last_5_minute_mesures_prom = 0   
+    end
+
+    #TODAS LAS MEDICIONES
     mesures_count = mesures.count
     all_mesures_prom = mesures.sum(&:value)
     if mesures_count > 0
@@ -22,24 +35,18 @@ class MesuresController < ApplicationController
       all_mesures_prom = 0
     end  
 
-    last_5_minutes_mesures = Mesure.where(user_id: id).last(cant2)
-    last_5_minutes_mesures_count = last_5_minutes_mesures.count
-    last_5_minutes_mesures_prom = last_5_minutes_mesures.sum(&:value)
-    if (last_5_minutes_mesures_count != 0 && last_5_minutes_mesures_count < cant2)
-      cant2 = last_5_minutes_mesures_count
-    end  
-    last_5_minutes_mesures_prom /= cant2
-
+    #MEDICIÓN MAS ALTA
     max_mesure = mesures.maximum(:value)
+    
+    #JSON
     render json: {max_mesure: max_mesure,last_minute_mesures_prom: last_minute_mesures_prom,all_mesures_prom: all_mesures_prom,last_5_minutes_mesures_prom: last_5_minutes_mesures_prom,mesures: last_minute_mesures}
   end  
 
   def create_mesure
-  valor = params[:data]
-  id = params[:id]
-  @mesure = Mesure.create(value: valor,user_id: id)
-	
-  render json: {msj: 'Creado' }
-  #redirect_to mesures_path(user_id: id)
+    valor = params[:data]
+    id = params[:id]
+    @mesure = Mesure.create(value: valor,user_id: id)
+  	
+    render json: {msj: 'Creado' }
   end	
 end
